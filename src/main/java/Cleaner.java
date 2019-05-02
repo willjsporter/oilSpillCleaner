ab75c2c11fd930761230bd511f3d57b03a14bce4
Will Porter <willjsporter@hotmail.co.uk>
2019-05-02 22:54:42 +0100

    feat(cleaner): cleaner can iterate through whole direction list

--- src/main/java/Cleaner.java
+++ src/main/java/Cleaner.java
@@ -1,23 +1,25 @@
 import utils.Coordinates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cleaner {
 
     private Area area;
     private Coordinates position;
-    private List<Coordinates> directions;
+    private ArrayList<Coordinates> directions;
 
 
-    public Cleaner(Area area, Coordinates position, List<Coordinates> directions) {
+    public Cleaner(Area area, Coordinates position, ArrayList<Coordinates> directions) {
         this.area = area;
         this.position = position;
         this.directions = directions;
     }
 
-    public void move() {
+    public void nextMove() {
         if(!directions.isEmpty()) {
             position.add(directions.get(0));
+            directions.remove(0);
         }
     }
 
--- src/test/java/CleanerTest.java
+++ src/test/java/CleanerTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import utils.Coordinates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,20 +15,33 @@ public class CleanerTest {
     public void cleanerShouldStayStationaryIfNoDirectionsAreGiven() {
         var area = new Area();
         var startingPoint = new Coordinates(0, 0);
-        Cleaner cleaner = new Cleaner(area, startingPoint, List.of() );
+        Cleaner cleaner = new Cleaner(area, startingPoint, new ArrayList<>());
 
-        cleaner.move();
+        cleaner.nextMove();
         assertThat(cleaner.getPosition(), is(startingPoint));
     }
 
     @Test
-    public void cleanerShouldChangePositionIfOneDirectionInputIsGiven() {
+    public void cleanerShouldMoveBasedOnListOfDirections() {
         var area = new Area();
         var startingPoint = new Coordinates(0, 0);
-        var directions = List.of(new Coordinates(1, 0));
+        var directions = new ArrayList<Coordinates>(List.of(
+                new Coordinates(1, 0),
+                new Coordinates(2, 1),
+                new Coordinates(-1, 5)
+        ));
+
         Cleaner cleaner = new Cleaner(area, startingPoint, directions);
 
-        cleaner.move();
+        cleaner.nextMove();
         assertThat(cleaner.getPosition(), equalTo(new Coordinates(1, 0)));
+
+        cleaner.nextMove();
+        assertThat(cleaner.getPosition(), equalTo(new Coordinates(3, 1)));
+
+        cleaner.nextMove();
+        assertThat(cleaner.getPosition(), equalTo(new Coordinates(2, 6)));
     }
+
+
 }
