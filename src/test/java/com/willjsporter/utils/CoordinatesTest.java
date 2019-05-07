e474cc4be3783af5e3717e40e29839f1ab14eaa4
Will Porter <willjsporter@hotmail.co.uk>
2019-05-08 00:55:34 +0100

    feat(coordinates): introduce method to turn list to coordinates

--- src/test/java/com/willjsporter/utils/CoordinatesTest.java
+++ src/test/java/com/willjsporter/utils/CoordinatesTest.java
@@ -2,8 +2,13 @@ package com.willjsporter.utils;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static com.willjsporter.utils.Coordinates.toCoordinates;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
 
 
 public class CoordinatesTest {
@@ -16,6 +21,22 @@ public class CoordinatesTest {
         coordinates1.moveBy(coordinates2);
 
         assertThat(coordinates1, is(new Coordinates(3,4)));
+    }
+
+    @Test
+    public void shouldBeAbleToConvert2ItemListToCoordinates() throws Exception {
+        var listToConvert = List.of(1,2);
+        assertThat(toCoordinates(listToConvert), equalTo(new Coordinates(1,2)));
+    }
+
+    @Test
+    public void listofLengthMoreThan2ShouldThrowException() {
+        try {
+            toCoordinates(List.of(1,2,3));
+            fail();
+        } catch (Exception e) {
+            assertThat(e.getMessage(), is("list must be of length 2 but was of length 3"));
+        }
     }
 
 }
