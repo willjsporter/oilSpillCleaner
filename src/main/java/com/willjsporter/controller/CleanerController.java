658b6ac1d24313d5ba834269eb639fc78bd30036
Will Porter <willjsporter@hotmail.co.uk>
2019-05-12 21:12:51 +0100

    feat(controller): controller now returns a cleaner based on the instructions

--- src/main/java/com/willjsporter/controller/CleanerController.java
+++ src/main/java/com/willjsporter/controller/CleanerController.java
@@ -1,5 +1,6 @@
 package com.willjsporter.controller;
 
+import com.willjsporter.Cleaner;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CleanerController {
 
-    @PostMapping(value = "/sendInstructions", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Instructions sendAndActionCleaner(@RequestBody Instructions instructions) {
-        return instructions;
+    @PostMapping(
+            value = "/sendInstructions",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE
+    )
+    public Cleaner sendAndActionCleaner(@RequestBody Instructions instructions) {
+        return instructions.createCleaner();
     }
 }
--- src/test/java/com/willjsporter/controller/CleanerControllerTest.java
+++ src/test/java/com/willjsporter/controller/CleanerControllerTest.java
@@ -1,5 +1,9 @@
 package com.willjsporter.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.willjsporter.Area;
+import com.willjsporter.Cleaner;
+import com.willjsporter.utils.Coordinates;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +18,12 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,19 +34,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class CleanerControllerTest {
 
-
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
-    public void controllerShouldProcessInstructionsAndSendBackFinalPositionAndNumberCleaned () throws Exception {
-        String instructions = readFileAsString("test_data.json");
-        this.mockMvc.perform(post("/sendInstructions").contentType(MediaType.APPLICATION_JSON_UTF8).content(instructions))
+    public void controllerShouldProcessInstructionsAndSendBackAppropriateCleaner () throws Exception {
+        String instructionsString = readFileAsString("test_data.json");
+
+        String controllerResponse = this.mockMvc.perform(post("/sendInstructions").contentType(MediaType.APPLICATION_JSON_UTF8).content(instructionsString))
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("areaSize")))
-                .andExpect(content().string(containsString("startingPosition")))
-                .andExpect(content().string(containsString("oilPatches")))
-                .andExpect(content().string(containsString("navigationInstructions")));
+                .andExpect(content().string(containsString("area")))
+                .andExpect(content().string(containsString("position")))
+                .andExpect(content().string(containsString("directions")))
+                .andExpect(content().string(containsString("totalSpillsCleaned")))
+                .andReturn().getResponse().getContentAsString();
+
+        Cleaner generatedCleaner = objectMapper.readValue(controllerResponse, Cleaner.class);
+        Cleaner expectedCleaner = new Cleaner(
+                new Area(5,5, List.of(new Coordinates(1,0), new Coordinates (2,0))),
+                new Coordinates(0,0),
+                new ArrayList<>(List.of(new Coordinates(0,1), new Coordinates(1,0), new Coordinates(0,-1), new Coordinates(1,0), new Coordinates(1,0), new Coordinates(0,1), new Coordinates(0,1)))
+        );
+
+        assertThat(generatedCleaner, equalTo(expectedCleaner));
     }
 
     private String readFileAsString(String pathToFile) throws URISyntaxException, IOException {
@@ -46,7 +67,6 @@ public class CleanerControllerTest {
 
         Path path = Paths.get(getClass().getClassLoader().getResource("test_data.json").toURI());
         byte[] fileBytes = Files.readAllBytes(path);
-        String data = new String(fileBytes);
-        return data;
+        return new String(fileBytes);
     }
 }
