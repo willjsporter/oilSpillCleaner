5b1bd3bb366fad04d9c8fb523e55184940298c96
will <willjsporter@hotmail.co.uk>
2019-05-16 11:48:13 +0100

    docs(controller-test): updated reference to helper method from spring website

--- src/test/java/com/willjsporter/controller/CleanerControllerTest.java
+++ src/test/java/com/willjsporter/controller/CleanerControllerTest.java
@@ -42,7 +42,7 @@ public class CleanerControllerTest {
     }
 
     private String readFileAsString(String pathToFile) throws URISyntaxException, IOException {
-        //implementation from spring docs: https://www.baeldung.com/reading-file-in-java
+        //implementation from spring docs: https://www.baeldung.com/reading-file-in-java#JDK7
 
         Path path = Paths.get(getClass().getClassLoader().getResource(pathToFile).toURI());
         byte[] fileBytes = Files.readAllBytes(path);
